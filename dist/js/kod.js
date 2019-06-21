526b389b8b0b0d85adfa3fefae369f415105eada
moshuying <1460083332@qq.com>
2019-06-21 11:52:25 +0800

    构建轮播图

--- dist/js/kod.js
+++ dist/js/kod.js
@@ -1,5 +1,37 @@
-var imgbox = document.getElementsByTagName('imgbox');
-function setImg(ix){
-    imgbox[0].style
+var imgbox = document.getElementsByClassName('imgbox');
+var Lbox=document.getElementById('Lbox');
+
+function setImg(){
+    for(var ix=0;ix<imgbox.length;ix++){
+        imgbox[ix].style.backgroundImage = setBgimgStr(ix);
+    }
 }
-console.log(imgbox[0])
+
+function setBgimgStr(ix){
+    var str=ix+1+"";
+    return "url('./assets/images/夜景 ("+str+").jpg"
+}
+
+function goNextPage(ix){
+    if(ix>=imgbox+1){
+        ix=000;
+    }
+    let then=ix+"%";
+    Lbox.style.right=then;
+}
+function auto(){
+    for (let ix = 0; ix < imgbox.length; ix++) {
+        
+    }
+}
+window.onload=function (){
+    setImg();
+    setInterval(goNextPage(200),1000);
+}
+
+function sleep(delay) {
+    var start = (new Date()).getTime();
+    while ((new Date()).getTime() - start < delay) {
+      continue;
+    }
+}
